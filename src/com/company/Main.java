517fde0a4966547428ef080b935ddd2d8cbd3aab
Alfonso Martinez Bernardo <alfonso.martinez.ext@entelgy.com>
2021-07-15 01:47:31 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("-> Brn Chispas ");
+        System.out.println("-------> @Alfon Mtz");
+        System.out.println("------------> Hello words");
+    }
+}
